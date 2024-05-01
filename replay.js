8a8b5a634704ca5250e6507545b0eb4cae4ed896
Scoronosar0K0 <166730359+Scoronosar0K0@users.noreply.github.com>
2024-05-01 13:37:31 +0800

    hw2

--- replay.js
+++ replay.js
@@ -1,14 +1,18 @@
-export const initReplayClickListener = (UserComment, Users, ListElement, rerender) => {
+export const initReplayClickListener = (
+    UserComment,
+    Users,
+    ListElement,
+    rerender,
+) => {
+    const commentsToAnswer = document.querySelectorAll('.comment')
 
-  const commentsToAnswer = document.querySelectorAll(".comment")
-  
-  for (const commentToAnswer of commentsToAnswer) {
-    commentToAnswer.addEventListener("click", () => {
-      const index = commentToAnswer.querySelector('.like-button').dataset.index
-      UserComment.value = `${Users[index].text}\n${Users[index].author}\n\n`
-    })
-  }
+    for (const commentToAnswer of commentsToAnswer) {
+        commentToAnswer.addEventListener('click', () => {
+            const index =
+                commentToAnswer.querySelector('.like-button').dataset.index
+            UserComment.value = `${Users[index].text}\n${Users[index].author}\n\n`
+        })
+    }
 
-  false && rerender(ListElement, Users)
-
+    false && rerender(ListElement, Users)
 }
